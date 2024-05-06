9963d54c86cac52c415eb673fb39f91b50f36131
Jasczlv <ianis.cozlov@gmail.com>
2024-05-06 14:54:38 +0200

    ADD bootstrap

--- index.php
+++ index.php
@@ -47,10 +47,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <title>Document</title>
 </head>
 <body>
-    <div>
+    <div class="card d-flex justify-content-center align-items-center">
         <?php 
          foreach($hotels as $index => $hotel){
             foreach($hotel as $hotel_dati){
