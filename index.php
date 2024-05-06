62ef6286f4d973b990834188374eaaa2c399b935
Jasczlv <ianis.cozlov@gmail.com>
2024-05-06 14:48:00 +0200

    ADD stampa in pagina

--- index.php
+++ index.php
@@ -41,3 +41,24 @@
     ];
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <?php 
+         foreach($hotels as $index => $hotel){
+            foreach($hotel as $hotel_dati){
+                echo "<span>" . $hotel_dati . "</span>". "<br>";
+
+            }
+         }
+        ?>
+    </div>
+</body>
+</html>
